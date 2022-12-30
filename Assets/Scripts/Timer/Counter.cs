6e987f4e00672e4d840147a539b1990050de06ee
Dragyn85 <nygard.jonas@gmail.com>
2022-12-30 07:41:10 +0100

    Updated Project unity version and structured files
    updated to Unity version 2021.3.15f1
    made new folders for the files to be ready for new test
    updated counter to show ms between frames

---
+++ Assets/Scripts/Timer/Counter.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Diagnostics;
+using System.Collections;
+
+public class Counter : MonoBehaviour
+{
+
+    public float StartTime = 1f;
+
+    private Stopwatch sp;
+    private long total;
+    private long num;
+    private long last;
+
+    private void Awake()
+    {
+        sp = new Stopwatch();
+        StartCoroutine(log());
+    }
+
+    private void Update()
+    {
+        if (Time.time < StartTime)
+        {
+            return;
+        }
+        RecordFrameTicks();
+        ResetStopWatch();
+    }
+
+    private void RecordFrameTicks()
+    {
+        if (sp.IsRunning)
+        {
+            sp.Stop();
+            num++;
+            last = sp.ElapsedTicks;
+            total += last;
+
+        }
+    }
+
+    private void ResetStopWatch()
+    {
+        sp.Reset();
+        sp.Start();
+    }
+
+    private IEnumerator log()
+    {
+        var delay = new WaitForSeconds(1f);
+        while (true)
+        {
+            yield return delay;
+            if (num > 0)
+            {
+                UnityEngine.Debug.Log(
+                    "Last time: " + (float)last / Stopwatch.Frequency * 1000f + "ms. " +
+                    "Average time: " + (float)(total / num) / Stopwatch.Frequency * 1000f + "ms.");
+            }
+        }
+    }
+
+}
